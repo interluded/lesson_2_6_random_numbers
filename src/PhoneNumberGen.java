1f205ecc39278661cec7f8af25d230cf3b20611b
marclmao <119444616+marclmao@users.noreply.github.com>
2023-10-25 10:08:21 -0400

    PhoneNumberGen.java

--- src/PhoneNumberGen.java
+++ src/PhoneNumberGen.java
@@ -8,7 +8,7 @@ public class PhoneNumberGen {
         int random_1 = (int) (100 + Math.random() * (999 - 100 + 1));
         int random_2 = (int) (100 + Math.random() * (999 - 100 + 1));
         int random_3 = (int) (1000 + Math.random() * (9999 - 1000 + 1));
-        System.out.println("(" + random_1 + ")" + "-" + random_2 + "-" + random_3);
+        System.out.println("The random phone number is: " + "(" + random_1 + ")" + "-" + random_2 + "-" + random_3);
         }
 
 
